503b4b8217f2029035d82a615cf7448fd8f3c563
Dragon-Keeper <44353148+Dragon-Keeper@users.noreply.github.com>
2022-11-09 20:06:46 +0800

    update

--- 激光测距/Laser_1306/Laser_1306.ino
+++ 激光测距/Laser_1306/Laser_1306.ino
@@ -36,10 +36,11 @@ Adafruit_SSD1306 display(OLED_RESET);
 //#error("Height incorrect, please fix Adafruit_SSD1306.h!");
 //#endif
 
+
 //控制屏幕方向-->0:正常显示；1：向右旋转90度；2：屏幕翻转180度；3：屏幕旋转270度。
-int Rotation = 0; //初始化屏幕方向
+//int Rotation = 0; //初始化屏幕方向
 //定义接受端口D8用于每摁一次屏幕旋转90度
-int Rotation_Screen = 15;
+//int Rotation_Screen = 15;
 int TextSize = 3;
 int DrawWight = 124;
 float distan;
@@ -74,34 +75,53 @@ void setup()
 
 #if defined LONG_RANGE
   // lower the return signal rate limit (default is 0.25 MCPS)
-  sensor.setSignalRateLimit(0.1);
+  /*
+  設定傳回訊號速率上限，單位是MCPS（每秒百萬次計數）。這是為了有效解讀目標物反射訊號所需的最低速率設定。
+  較低的設定值可增加感測範圍，但也可能因增加讀取到目標以外的物體的反射訊號，而導致准確率下滑。
+  訊號速率限制的初始預設值為0.25 MCPS。此函式將傳回一個bool值，代表設定值是否有效。
+  */
+  //sensor.setSignalRateLimit(0.1);
+  sensor.setSignalRateLimit(0.25);
   // increase laser pulse periods (defaults are 14 and 10 PCLKs)
-  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  /*
+    設定成指定的週期時脈值。較長的週期時間可加長感測距離。有效值為（僅偶數）：
+    Pre：12到18（預設為14）
+    Final：8到14（預設為10）
+    傳回值是bool類型，指出設定的週期時間是否有效。
+  */
+  //sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  //sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 14);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 10);
 #endif
 
 #if defined HIGH_SPEED
   // reduce timing budget to 20 ms (default is about 33 ms)
+  /*
+  設定測量資料更新時間（單位是微秒）。這是一次測距所允許的時間；較長的資料更新時間可獲得更準確的測量。
+  資料更新時間約為33000微秒，也就是33毫秒；最低為20毫秒。傳回值是bool類型，代表設定的資料更新時間是否有效。
+  */
   sensor.setMeasurementTimingBudget(20000);
 
 #elif defined HIGH_ACCURACY
   // increase timing budget to 200 ms
   sensor.setMeasurementTimingBudget(200000);
 #endif
+  // initialize with the I2C addr 0x3C (for the 128x64) ，0x3C为I2C协议通讯地址，需根据实际情况更改
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64) ，0x3C为I2C协议通讯地址，需根据实际情况更改
-
-  pinMode(Rotation_Screen, INPUT_PULLUP); //设置管脚为旋转屏幕信号输入上拉，令到它值为HIGH
+  //pinMode(Rotation_Screen, INPUT_PULLUP); //设置管脚为旋转屏幕信号输入上拉，令到它值为HIGH
 }
 
 void loop()
 {
-  Serial.println(sensor.readRangeSingleMillimeters()); //单次测量
+  Serial.println(sensor.readRangeSingleMillimeters()); //单次测量并传回单位（mm）的测量值
   if (sensor.timeoutOccurred())
   {
     Serial.println(F(" TIMEOUT"));
   }
 
+   /*
   //控制屏幕方向
   if (digitalRead(Rotation_Screen) < 1)
   {
@@ -123,12 +143,14 @@ void loop()
     TextSize = 2;  //正反向时字体大小为2
     DrawWight = 60;  //正反向时动态实心矩形宽60
   }
+  */
 
   //算术平均滤波法
-  distan = Filter() - 4.3;  //结果减去4.3cm的偏移量得正确测量值
+  distan = Filter() - 4 ;  //结果减去4.3cm的偏移量得正确测量值
+  //distan = Filter();  //结果减去4.3cm的偏移量得正确测量值
   /*------------------------显示距离---------------------*/
   display.clearDisplay();   //显示之前清屏
-  display.setRotation(Rotation);  //旋转屏幕
+  //display.setRotation(Rotation);  //旋转屏幕
   display.setTextSize(TextSize);  //选择字号
   display.setTextColor(WHITE);  //字体颜色
   display.setCursor(0, 10);  //字体位置
@@ -139,17 +161,16 @@ void loop()
   display.fillRect(2, 40, map(distan, 2, 200, 2, DrawWight), 8, WHITE);  //map(要映射的值，旧区间初值，旧区间终值，新区间初值，新区间终值)
   //进度条采用静态空心矩形套一个动态实心矩形实现//
   display.display();  //显示图形
-  delay(510);
 }
 
 // 算术平均滤波法
-#define FILTER_N 10
-float Filter() {
+#define FILTER_N 8
+float Filter() 
+{
   int i;
   float filter_sum = 0;
   for (i = 0; i < FILTER_N; i++) {
     filter_sum += sensor.readRangeSingleMillimeters();
-    //delay(210);
   }
   return (float)(filter_sum / FILTER_N / 10);
 }
